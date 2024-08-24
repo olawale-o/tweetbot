4409b5b0270d3b7c8bba923741211929692ba794
olawale-o <omoogunolawale@gmail.com>
2024-08-24 06:53:07 +0100

    Display bot count every hour

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BotCount />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BotCount from "./components/BotCount";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BotCount,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/BotCount/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <div>Bot counter {{ counter }}</div>
+    <div>Bot Count {{ counts }}</div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+export default {
+  name: "BotCount",
+  props: {
+    msg: String,
+  },
+  setup() {
+    const counts = ref(0);
+    const handleSSEMessage = (e) => {
+      counts.value = JSON.parse(e.data).counts[0].value;
+    };
+    onMounted(() => {
+      const eventSource = new EventSource("http://localhost:3000/stream");
+      eventSource.addEventListener("message", handleSSEMessage);
+    });
+    return {
+      counts,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
